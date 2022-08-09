61db5dbbb04e2cae95d33358ca5be678d2413d04
Julian Pedroza <julianpedrozagarcia@gmail.com>
2022-08-09 14:21:34 -0500

    adding the daysDiff and getKNDWeek functions, getKNDWeek still in progress

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -1,6 +1,8 @@
-//date = mm dd aaaa
-var date = new Date("august-8-2022");
+//vars and conversor objects
 var now = new Date();
+var curMonth;
+const referenceDay = new Date(2022,7,7); //Aug 7 2022
+const referenceKNDWeek = 3;
 const weekDays = {
   0: "sunday",
   1: "monday",
@@ -24,8 +26,126 @@ const months = {
   10: "November",
   11: "December"
 };
-console.log("Date: "+date);
-console.log("Week day "+ weekDays[date.getDay()]);
-console.log("Day "+parseInt(date.toString().split(" ")[2]));
-console.log("Month "+months[date.getMonth()]);
-console.log("Year "+date.getFullYear());
+const KNDweeks = [
+  {
+    week: 1,
+    monday: {
+      teams: ["K","F","F"],
+      place: "Farallones"
+    },
+    tuesday: {
+      teams: ["K","A","A"],
+      place: "Buenos Aires"
+    },
+    wednesday: {
+      teams: ["K","M","M"],
+      place: "Farallones"
+    },
+    thursday: {
+      teams: ["A","A","A"],
+      place: "Buenos Aires"
+    },
+    friday: {
+      teams: ["K","F","M"],
+      place: "Farallones"
+    },
+    saturday: {
+      teams:["K"],
+      place: "Farallones"
+    }
+  },
+  {
+    week: 2,
+    monday: {
+      teams: ["K","M","M"],
+      place: "Farallones"
+    },
+    tuesday: {
+      teams: ["K","A","A"],
+      place: "Buenos Aires"
+    },
+    wednesday: {
+      teams: ["K","F","M"],
+      place: "Farallones"
+    },
+    thursday: {
+      teams: ["A","A","A"],
+      place: "Buenos Aires"
+    },
+    friday: {
+      teams: ["K","F","F"],
+      place: "Farallones"
+    },
+    saturday: {
+      teams:["K"],
+      place: "Farallones"
+    }
+  },
+  {
+    week: 3,
+    monday: {
+      teams: ["K","F","F"],
+      place: "Farallones"
+    },
+    tuesday: {
+      teams: ["K","A","A"],
+      place: "Buenos Aires"
+    },
+    wednesday: {
+      teams: ["K","F","M"],
+      place: "Farallones"
+    },
+    thursday: {
+      teams: ["A","A","A"],
+      place: "Buenos Aires"
+    },
+    friday: {
+      teams: ["K","M","M"],
+      place: "Farallones"
+    },
+    saturday: {
+      teams:["K"],
+      place: "Farallones"
+    }
+  },
+];
+const dayPrototype = function(date, teams, place, KNDWeek){
+  let month = date.getMonth+1;
+  
+  this.date = new Date()
+  this.day = parseInt(date.toString().split(" ")[2]);
+  this.KNDWeek = KNDWeek;
+  this.weekDay = date.getDay();
+  this.time1 = teams[0];
+  this.time2 = teams[1];
+  this.place = place;
+}
+const monthPrototype = function(date){
+  this.name = months[date.getMonth()];
+  this.year = date.getFullYear();
+  this.days = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+};
+
+//testing with dates
+console.log("Date: "+now);
+console.log("Week day "+ weekDays[now.getDay()]);
+console.log("Day "+parseInt(now.toString().split(" ")[2]));
+console.log("Month "+months[now.getMonth()]);
+console.log("Year "+now.getFullYear());
+
+/*functions*/
+function daysDiff(date){
+  //Days difference from referenceday
+  let curDate = new Date(date.getFullYear(),date.getMonth(),parseInt(date.toString().split(" ")[2]));
+  return (curDate-referenceDay)/86400000;
+}
+function getKNDWeek(date){
+  let days = daysDiff(date);
+  if(days >= 0){
+    let weekDiff = Math.floor(days/7);
+    return weekDiff%3 >0 ? weekDiff%3 : 3;
+  }else{
+    let weekDiff = Math.floor()
+    return "negative days";
+  }
+}
